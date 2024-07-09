5ebd624bf71f968d649cf03bba16725b93bfc566
LB05-8 <ncedenob@uteq.edu.ec>
2024-07-09 09:15:33 -0500

    mapaa

--- app/src/main/java/com/example/mapa/MainActivity.java
+++ app/src/main/java/com/example/mapa/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.mapa;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -19,12 +21,23 @@ import com.google.android.gms.maps.model.Circle;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.slider.Slider;
 
 public class MainActivity
         extends AppCompatActivity
         implements OnMapReadyCallback {
 
     GoogleMap mapa;
+
+    Double lat, lng;
+
+    EditText txtLat, txtLong;
+
+    Circle circulo = null;
+
+    Slider sliderRadio;
+
+    float radio=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +54,22 @@ public class MainActivity
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
         .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        txtLat = findViewById(R.id.editTextNumberDecimal);
+        txtLong = findViewById(R.id.editTextNumberDecimal2);
+        sliderRadio = findViewById(R.id.slider);
 
+        sliderRadio.addOnSliderTouchListener(new Slider.OnSliderTouchListener() {
+            @Override
+            public void onStartTrackingTouch(@NonNull Slider slider) {
+                radio = slider.getValue();
+                updateInterfaz();
+            }
+
+            @Override
+            public void onStopTrackingTouch(@NonNull  Slider slider) {
+
+            }
+        });
     }
 
     @Override
@@ -70,6 +98,37 @@ public class MainActivity
         Circle circulo = mapa.addCircle(circleOptions);
 
 
+        mapa.setOnCameraIdleListener(new GoogleMap.OnCameraIdleListener() {
+            @Override
+            public void onCameraIdle() {
+                LatLng center = mapa.getCameraPosition().target;
+                lat = center.latitude;
+                lng =center.longitude;
+                updateInterfaz();
+
+            }
+        });
+
+
 
     }
+    private void updateInterfaz(){
+        txtLat.setText(String.format("%.4f", lat));
+        txtLong.setText(String.format("%.4f", lng));
+
+        //PintarCirculo()
+    }
+
+    private void PintarCirculo(){
+
+    CircleOptions circleOptions = new CircleOptions()
+            .center(new LatLng(lat,lng))
+            .radius(radio*100) //En Metros
+            .strokeColor(Color.RED)
+            .fillColor(Color.argb(50, 150, 50, 50));
+
+    circulo = mapa.addCircle(circleOptions);
+
+    }
+
 }
