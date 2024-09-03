f8068b1372961466f05e151c42ca15b9533af549
Walter-Cadme <wcadmez@uteq.edu.ec>
2024-09-02 22:55:52 -0500

    grupo1

--- app/src/main/java/com/example/mapa/MainActivity.java
+++ app/src/main/java/com/example/mapa/MainActivity.java
@@ -1,134 +1,64 @@
 package com.example.mapa;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
+import com.example.gmaps.adaptador.MyInfoAdapter;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-
-import com.google.android.gms.maps.CameraUpdate;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.model.Circle;
-import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.material.slider.Slider;
 
-public class MainActivity
-        extends AppCompatActivity
-        implements OnMapReadyCallback {
-
-    GoogleMap mapa;
-
-    Double lat, lng;
-
-    EditText txtLat, txtLong;
-
-    Circle circulo = null;
-
-    Slider sliderRadio;
-
-    float radio=1;
-
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback,
+        GoogleMap.OnMapClickListener {
+    private GoogleMap mMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-
-
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-        .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-        txtLat = findViewById(R.id.editTextNumberDecimal);
-        txtLong = findViewById(R.id.editTextNumberDecimal2);
-        sliderRadio = findViewById(R.id.slider);
-
-        sliderRadio.addOnSliderTouchListener(new Slider.OnSliderTouchListener() {
-            @Override
-            public void onStartTrackingTouch(@NonNull Slider slider) {
-                radio = slider.getValue();
-                updateInterfaz();
-            }
-
-            @Override
-            public void onStopTrackingTouch(@NonNull  Slider slider) {
-
-            }
-        });
+        initMap();
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mapa= googleMap;
-
-        mapa.getUiSettings().setZoomControlsEnabled(true);
-        CameraUpdate camUpd1 = CameraUpdateFactory
-                .newLatLngZoom(new LatLng(-1.02372, -79.46519), 13);
-                        mapa.moveCamera(camUpd1);
-
-                        //PUNTO DE LA UTEQ
-
-        LatLng punto = new LatLng(-1.01292, -79.46890);
-        mapa.addMarker(new MarkerOptions().position(punto)
-                .title("Marker in Sydney"));
-
-        //CIRCULO DE LA UTEQ
-
-        CircleOptions circleOptions = new CircleOptions()
-                .center(punto)
-                .radius(200) //En Metros
-                .strokeColor(Color.RED)
-                .fillColor(Color.argb(50, 150, 50, 50));
-
-        Circle circulo = mapa.addCircle(circleOptions);
-
-
-        mapa.setOnCameraIdleListener(new GoogleMap.OnCameraIdleListener() {
-            @Override
-            public void onCameraIdle() {
-                LatLng center = mapa.getCameraPosition().target;
-                lat = center.latitude;
-                lng =center.longitude;
-                updateInterfaz();
-
-            }
-        });
-
-
-
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+        setupMapFeatures();
     }
-    private void updateInterfaz(){
-        txtLat.setText(String.format("%.4f", lat));
-        txtLong.setText(String.format("%.4f", lng));
-
-        //PintarCirculo()
+    @Override
+    public void onMapClick(@NonNull LatLng latLng) {
     }
-
-    private void PintarCirculo(){
-
-    CircleOptions circleOptions = new CircleOptions()
-            .center(new LatLng(lat,lng))
-            .radius(radio*100) //En Metros
-            .strokeColor(Color.RED)
-            .fillColor(Color.argb(50, 150, 50, 50));
-
-    circulo = mapa.addCircle(circleOptions);
-
+    private void initMap() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
+    private void setupMapFeatures() {
+        //mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        LatLng UTEQLocation = new LatLng(-1.0109242630680952, -79.46842649490726);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(UTEQLocation, 16));
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
+        addMapMarker(new LatLng(-1.01350399309634, -79.46815845233768), "CAFE KIMO", "Cafeteria", R.mipmap.ic_cafe);
+        addMapMarker(new LatLng(-1.0108414790287659, -79.47047791799375), "KONITOS PIZZA", "Pizza", R.mipmap.ic_pizza);
+        addMapMarker(new LatLng(-1.0116926846991625, -79.4668864858666), "BUNKER FRUIT", "Restaurante", R.mipmap.ic_heladito);
+        addMapMarker(new LatLng(-1.0116926846991625, -79.46668349447533),"Encebollado Pique y Pase","Restaurante", R.mipmap.ic_restaurant);
+        addMapMarker(new LatLng(-1.0134964709572454, -79.46930492745352),"Lokos D' Asar","Restaurante", R.mipmap.ic_restaurant);
+        addMapMarker(new LatLng(-1.0141128250964173, -79.46832758691284),"Sweet Land - Quevedo","Heladeria", R.mipmap.ic_heladito);
+        addMapMarker(new LatLng(-1.0141242636177403, -79.46831397754428),"Las Delicias Del Olimpo","Restaurante", R.mipmap.ic_cafe);
+        addMapMarker(new LatLng(-1.0098749690620858, -79.46939479855095),"Pizza Hut Quevedo","Restaurante", R.mipmap.ic_pizza);
+        mMap.setInfoWindowAdapter(new MyInfoAdapter(this));
+    }
+    private void addMapMarker(LatLng pos, String titleText, String detailText, int resourceIcon) {
+        MarkerOptions marker = new MarkerOptions()
+                .position(pos)
+                .title(titleText)
+                .snippet(detailText)
+                .icon(BitmapDescriptorFactory.fromResource(resourceIcon));
+        mMap.addMarker(marker);
+    }
 }
